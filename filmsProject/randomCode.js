fbbf2cd28eda5976b2454272cc3e1c57ec35b364
jiri <jiri.soloduk@gmail.com>
2021-09-28 16:19:32 +0200

    added some functions to the film database

---
+++ filmsProject/randomCode.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const str = "test";
+
+// const arr = [1, 2, 4];
+
+console.log(str.toUpperCase());
+console.log(str[2]);
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "hello world";
+
+// console.log(logg.slice(-6, -1));
+
+console.log(logg.substr(6, 5));
+
